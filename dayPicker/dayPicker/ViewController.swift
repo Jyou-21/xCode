9a69e0f96e6fcfa947321a398b7f11889a6e0205
Jack Y <jack.y21@k12.xrds.org>
2019-10-27 19:53:22 -0700

    Added afternoon classes

--- dayPicker/dayPicker/ViewController.swift
+++ dayPicker/dayPicker/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
     }
     
     @IBOutlet weak var dayPicker: UIPickerView!
+    @IBOutlet weak var finalView: UILabel!
     
     let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     let block = ["A", "B", "C"]
     let schedType = ["Regular", "Double Block", "Assembly"]
-    
     let mySchedule: [String:String] = ["A":"Free Period",
                                        "B":"Computer Science",
                                        "C":"Trig/Pre-Calc",
@@ -32,7 +32,6 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0{
             return block.count
-            
         }else if component == 1 {
             return day.count
         }else {
@@ -51,6 +50,35 @@ class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSou
         }
     }
     
+    @IBAction func getSchedule(_ sender: Any) {
+        let blockTypeRow = dayPicker.selectedRow(inComponent: 0)
+        let ACBDay = block[blockTypeRow]
+     
+        let weekDayRow = dayPicker.selectedRow(inComponent: 1)
+        let weekDay = day[weekDayRow]
+        
+        let periodRotation:[String:[String]] = [
+            "A":["A","B","Snack","D","E","Lunch"],
+            "C":["C","A","Snack","F","D","Lunch"],
+            "B":["B","C","Snack","E","F","Lunch"]]
+        let mySched = getScheduleForDay(periods: periodRotation[ACBDay]!, mySchedule:mySchedule, weekDay: weekDay)
+        finalView.text = mySched
+    }
+    
+    func getScheduleForDay(periods:[String], mySchedule:[String:String],weekDay:String) -> String{
+        var output = ""
+        for period in periods{
+            output += "\(period) - "
+            output += mySchedule[period] ?? "free period"
+            output += "\n"
+        }
+        if weekDay == "Monday" || weekDay == "Wednesday"{
+            output.append("DB - Graphic Design")
+        }else if weekDay == "Friday"{
+            output.append("DB - Life Skills")
+        }
+        return output
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
