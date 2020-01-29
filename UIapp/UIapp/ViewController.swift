e12df35539006f03e533f49a27e42eb4011a26fb
Jack Y <jack.y21@k12.xrds.org>
2020-01-29 10:04:48 -0800

    UWideApp

---
+++ UIapp/UIapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIapp
+//
+//  Created by Jack Younger on 1/29/20.
+//  Copyright © 2020 Jack Younger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
